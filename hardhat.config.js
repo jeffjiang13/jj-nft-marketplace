fe450830b2f4bb04f06c57861f75e070ae207c87
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-02 17:33:33 -0500

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,11 +13,10 @@ module.exports = {
       url: 'https://rpc-mumbai.maticvigil.com',
       accounts: [privateKey],
     },
-    // rinkeby: {
-    // url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
-    //   accounts: [privateKey],
-    // },
+    rinkeby: {
+    url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
+      accounts: [privateKey],
+    },
   },
   solidity: '0.8.4',
 };
-
